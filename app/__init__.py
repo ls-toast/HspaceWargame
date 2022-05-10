167fe6cf9404793284ca456bd8196ed2b4a0a9f2
ls-toast <leesak2004@gmail.com>
2022-05-10 23:08:12 +0900

    register template

--- app/__init__.py
+++ app/__init__.py
@@ -3,5 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 
 from app.web.index import main as main
+from app.web.register import register as register
 
 app.register_blueprint(main)
+app.register_blueprint(register)
