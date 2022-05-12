207e66cf8e04e3489f0f03173e7f19815686db49
ls-toast <leesak2004@gmail.com>
2022-05-13 00:26:48 +0900

    create login form

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,8 @@ app = Flask(__name__)
 
 from app.web.index import main as main
 from app.web.register import register as register
+from app.web.login import login as login
 
 app.register_blueprint(main)
 app.register_blueprint(register)
+app.register_blueprint(login)
